b2b323c6f28674bb96035126ee35facaa3c4caa0
lennardrk <lennardrk@outlook.com>
2016-12-19 15:02:24 +0100

    .

--- new-spark-project/src/main/java/Main.java
+++ new-spark-project/src/main/java/Main.java
@@ -3,19 +3,24 @@ import static spark.Spark.*;
 
 public class Main {
 
-
-    public static void main(String[] args) {
-        port(8000);
-        boolean admin = false;
-        get("/", (request, response) -> {
+    public static String url(boolean admin){
         if(admin == true) {
             return "Hello admin";
         }else{
             return "Hello user";
         }
-        });
+    }
+
+    public static void main(String[] args) {
+        port(80);
+
+
+        boolean admin = false;
+
+        get("/", (request, response) -> url(admin));
 
         get("/admin", (request, response) -> "Hello admin");
+
     }
 
 }
